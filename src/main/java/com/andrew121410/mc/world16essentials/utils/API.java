ae3136a59eeb469795bfb8b4c729da526dc5225f
Andrew121410 <andrewcherring@gmail.com>
2022-06-11 00:06:24 -0400

    Lol

--- src/main/java/com/andrew121410/mc/world16essentials/utils/API.java
+++ src/main/java/com/andrew121410/mc/world16essentials/utils/API.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 public class API {
 
     public static final String CUSTOM_COMMAND_FORMAT = "`";
-    public static final String DATE_OF_VERSION = "6/10/2022";
+    public static final String DATE_OF_VERSION = "6/11/2022";
 
     private final World16Essentials plugin;
 
