b07302f2c30c444fce4948c15ac2724a0ac2790e
Andrew121410 <andrewcherring@gmail.com>
2022-07-12 13:32:08 -0400

    Backport fix from master

--- src/main/java/com/andrew121410/mc/world16essentials/utils/API.java
+++ src/main/java/com/andrew121410/mc/world16essentials/utils/API.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 
 public class API {
 
-    public static final String DATE_OF_VERSION = "7/11/2022";
+    public static final String DATE_OF_VERSION = "7/12/2022";
     public static final String CUSTOM_COMMAND_FORMAT = "`";
 
     private final World16Essentials plugin;
