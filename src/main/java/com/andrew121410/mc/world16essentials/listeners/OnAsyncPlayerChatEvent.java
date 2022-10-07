154f0d3cc2860dce5a5acc5d41834b35caed1d21
Andrew121410 <andrewcherring@gmail.com>
2022-10-07 19:59:58 -0400

    Use AsyncChatEvent instead of AsyncPlayerChatEvent
    AsyncPlayerChatEvent is deprecated

--- src/main/java/com/andrew121410/mc/world16essentials/listeners/OnAsyncPlayerChatEvent.java
+++ src/main/java/com/andrew121410/mc/world16essentials/listeners/OnAsyncPlayerChatEvent.java
@@ -3,6 +3,9 @@ package com.andrew121410.mc.world16essentials.listeners;
 import com.andrew121410.mc.world16essentials.World16Essentials;
 import com.andrew121410.mc.world16essentials.utils.API;
 import com.andrew121410.mc.world16utils.chat.Translate;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -21,21 +24,22 @@ public class OnAsyncPlayerChatEvent implements Listener {
     }
 
     @EventHandler
-    public void ChatEvent(org.bukkit.event.player.AsyncPlayerChatEvent event) {
+    public void on(AsyncChatEvent event) {
         Player player = event.getPlayer();
-        String message = event.getMessage();
+        Component messageComponent = event.message();
+        String message = PlainTextComponentSerializer.plainText().serialize(messageComponent);
 
-        //Name ping
+        // Name ping
         this.plugin.getServer().getScheduler().runTask(this.plugin, () -> {
             for (Player targetPlayer : plugin.getServer().getOnlinePlayers()) {
                 if (message.contains(targetPlayer.getName())) {
-                    if (api.isAfk(targetPlayer)) {
-                        plugin.getServer().broadcastMessage(Translate.chat("&6[Afk] &eLooks like &r&9" + targetPlayer.getDisplayName() + "&r&e, is afk they may not respond."));
+                    if (this.api.isAfk(targetPlayer)) {
+                        this.plugin.getServer().broadcast(Translate.miniMessage("<gold><underline>" + targetPlayer.getName() + " is AFK!"));
                     }
                     targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 10.0f, 1.0f);
                 }
 
-                if (message.contains("!" + targetPlayer.getDisplayName())) {
+                if (message.contains("!" + targetPlayer.getName())) {
                     targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_HORSE_DEATH, 10.0f, 1.0f);
                 }
             }
