3a9a73834250b0713dd88ee1cefcdfe6e9ed2a59
Andrew121410 <andrewcherring@gmail.com>
2023-12-03 22:07:30 -0500

    [Backport] Remove broken players in LastJoin for GUI

--- src/main/java/com/andrew121410/mc/world16essentials/commands/playertime/LastJoinCMD.java
+++ src/main/java/com/andrew121410/mc/world16essentials/commands/playertime/LastJoinCMD.java
@@ -165,6 +165,9 @@ public class LastJoinCMD implements CommandExecutor {
             return;
         }
 
+        // Remove broken players
+        offlinePlayerList.removeIf(offlinePlayer -> offlinePlayer == null || offlinePlayer.getName() == null || offlinePlayer.getName().isEmpty() || offlinePlayer.getName().equals("null"));
+
         // Sort
         offlinePlayerList.sort((o1, o2) -> Long.compare(o2.getLastPlayed(), o1.getLastPlayed()));
 
