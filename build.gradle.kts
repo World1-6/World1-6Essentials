4af4d7c82723d2ad0987dc25399aad3877081122
Andrew121410 <andrewcherring@gmail.com>
2024-03-18 00:31:13 -0400

    Bump World1-6Utils

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,8 +70,8 @@ repositories {
 dependencies {
     api("org.bstats:bstats-bukkit:3.0.2")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils-Plugin:b6e8f2eacb")
-    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils_CMI_API:b6e8f2eacb")
+    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils-Plugin:0c7e98a7")
+    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils_CMI_API:0c7e98a7")
     compileOnly("net.essentialsx:EssentialsX:2.21.0-SNAPSHOT")
     compileOnly("org.geysermc.floodgate:api:2.2.2-SNAPSHOT")
 }
