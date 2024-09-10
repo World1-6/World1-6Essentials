e404c1a6a81cd066b08003d0d9979b4a82326376
Andrew121410 <andrewcherring@gmail.com>
2024-09-10 16:12:37 -0400

    [ci skip] Bump World1-6Utils

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,8 +81,8 @@ repositories {
 dependencies {
     api("org.bstats:bstats-bukkit:3.0.2")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils-Plugin:634da50bcc")
-    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils_CMI_API:634da50bcc")
+    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils-Plugin:0f7f4791dd")
+    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils_CMI_API:0f7f4791dd")
     compileOnly("net.essentialsx:EssentialsX:2.21.0-SNAPSHOT")
     compileOnly("org.geysermc.floodgate:api:2.2.3-SNAPSHOT")
 }
